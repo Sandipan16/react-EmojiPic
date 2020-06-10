37943c4264d1a9c4d938f9d0ebd16037adb92c30
Sandipan16 <50637809+Sandipan16@users.noreply.github.com>
2020-06-10 22:56:18 +0530

    commit 1
    Some bug fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import Entry from "./Entry";
 import List from "../emoji";
 import "../styles.css";
-
+var i = 1;
 export default function App() {
   return (
     <div className="App">
       <h1>emojipedia</h1>
       <h2>Beautiful fantastic picture of emojis</h2>
       {List.map(function(img) {
-        return <Entry img={img} />;
+        return <Entry key={i++} img={img} />;
       })}
     </div>
   );
